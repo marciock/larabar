e82b3c964a19446a14b932656c6734d67b388388
marciock <marciockalves@gmail.com>
2018-10-12 10:42:21 -0300

    V

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Lara Bar</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <!-- Styles -->
+       
+    </head>
+    <body>
+        
+
+            <div id="index">            
+
+               
+            </div>
+        </div>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
